a507d8af299810baceacc5dc33900ac6aba0f25e
Tadiwaaaaa <kapfidzetadiwanashe@gmail.com>
2025-08-20 10:02:02 +0200

    Added javascript

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert("Hello World");
+function generateVerse(event) {
+  event.preventDefault();
+
+
+  new Typewriter(".verse", {
+    strings: ["The Lord is my shepherd, I shall not want"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
+}
+
+let verseformElement = document.querySelector(".form");
+verseformElement.addEventListener("submit", generateVerse);

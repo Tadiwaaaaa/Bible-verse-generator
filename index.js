f0f6eac0668da5bf9db8b6e863bbcb60b9645761
Tadiwaaaaa <kapfidzetadiwanashe@gmail.com>
2025-08-25 09:16:30 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function generateVerse(event) {
 
   let apiKey = "bb9tdd6f8a38d9fb4774eo8499ffbe03";
   let prompt = `User Instruction: Generate a bible verse about ${instructionInput.value}`;
-  let context = "You are a Christian growing their knowledge and relationship with God. Your goal is to memorize as many bible verses as possible. Make sure to follow the user instruction to generate the correct bible verse in basic HTML. Encapsulate the whole verse in the <i></i> element";
+  let context = "You are a Christian growing their knowledge and relationship with God. Your goal is to memorize as many bible verses as possible. Make sure to follow the user instruction to generate the correct bible verse in basic HTML. Encapsulate the whole verse in the <i></i> element including the name of the verse";
   let apiUrl =
     `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 

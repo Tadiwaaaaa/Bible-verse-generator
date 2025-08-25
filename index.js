8228595824c299ad6f30f9b7d02c57f53ee72174
Tadiwaaaaa <kapfidzetadiwanashe@gmail.com>
2025-08-25 09:12:28 +0200

    integrated the API

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+function displayVerse(response){
+  console.log("poem generated");
+   new Typewriter(".verse", {
+     strings: response.data.answer,
+     autoStart: true,
+     delay: 1,
+     cursor: "",
+   });
+
+
+}
+
 function generateVerse(event) {
   event.preventDefault();
 
+  let instructionInput = document.querySelector(".instruction");
 
-  new Typewriter(".verse", {
-    strings: ["The Lord is my shepherd, I shall not want"],
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
+  let apiKey = "bb9tdd6f8a38d9fb4774eo8499ffbe03";
+  let prompt = `User Instruction: Generate a bible verse about ${instructionInput.value}`;
+  let context = "You are a Christian growing their knowledge and relationship with God. Your goal is to memorize as many bible verses as possible. Make sure to follow the user instruction to generate the correct bible verse in basic HTML. Encapsulate the whole verse in the <i></i> element";
+  let apiUrl =
+    `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log("generating verse ");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+
+
+    axios.get(apiUrl).then(displayVerse);
+
 
 }
 
